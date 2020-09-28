18b4278f1bb517369b8d2f81c456651827d30de4
Jochem Stoel <jochemstoel@gmail.com>
2020-09-28 09:00:05 +0200

    Attempt to fix sleep(0) error
    Someone reported sleep(0) crashes the script on MacOS, attempting to fix.

--- module.js
+++ module.js
@@ -1,12 +1,15 @@
 /* Sleep function for Node.
  * 
  * @package system-sleep
- * @version 1.0
+ * @version 1.3.7
  * @author Jochem Stoel (http://jochemstoel.github.io)
  * @license don't involve me
  */
 
 const sleep = interval => new Promise((deliver, renege) => {
+	if(typeof interval != 'number' || interval < 1) deliver(
+		new Date().getTime()
+	)
 	try {
 		setTimeout(() => {
 			deliver(
@@ -19,14 +22,17 @@ const sleep = interval => new Promise((deliver, renege) => {
 		)
 	}
 })
+
 module.exports = interval => {
+	if(typeof interval != 'number' || interval < 1) 
+		return new Date().getTime()
 	try {
 		return require('deasync-promise')(
 			sleep(interval)
 		)
 	} catch (notAsync) { /* https://github.com/jochemstoel/nodejs-system-sleep/issues/4 */
 		require('child_process').execSync(
-			`"${process.execPath}" -e "setTimeout(function () { return true; }, ${interval} );"`
+			`"${process.execPath}"` + " -e \"setTimeout(function () { return true; }, " + interval + ");\""
 		)
 		return null
 	}
